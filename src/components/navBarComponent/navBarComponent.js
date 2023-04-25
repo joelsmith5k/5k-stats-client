16ca231b8f717215d7a43abc2c7570d3ffa2faa9
JOEL_SMITH <joelsmith5000@gmail.com>
2023-04-24 21:29:28 -0700

    fixing routing

--- src/components/navBarComponent/navBarComponent.js
+++ src/components/navBarComponent/navBarComponent.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import "./navBarComponent.css";
-import { Disclosure, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Home', href: '/', current: true },
@@ -62,6 +61,15 @@ const NavBarComponent = () => {
                     ))}
                   </div>
                 </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
               </div>
             </div>
           </div>
