b93a40f6da084f4bb94f22c963448b43b9fc9f2b
JOEL_SMITH <joelsmith5000@gmail.com>
2023-05-06 09:00:37 -0700

    tailwind navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "./App.css";
 import PgaComponent from "./components/pgaComponent/pgaComponent";
 import NhlComponent from "./components/nhlComponent/nhlComponent";
 import NbaComponent from "./components/nbaComponent/nbaComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
 import HomePage from "./components/homePage/homePage";
 import NavBarComponent from "./components/navBarComponent/navBarComponent";
-import FooterComponent from "./components/footerComponent/footerComponent";
 
 const App = () => {
   return (
     <>
     <NavBarComponent />
-    <Routes>
-    
+    <Routes>   
        <Route path="/" element={<HomePage />} />
        <Route path="/pga" element={<PgaComponent />} />
        <Route path="/nba" element={<NbaComponent />} />
