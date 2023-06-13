dd61b393b5a7647a708fc74fafd49c9274e31539
JOEL_SMITH <joelsmith5000@gmail.com>
2023-06-12 22:35:16 -0700

    cleaning up homepage

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -10,25 +10,23 @@ function HomePage() {
   ];
 
   return (
-    <div className="homePageComponentContainer flex flex-col items-center">
-      <div className="flex flex-col h-24 justify-center">
+    <div className="homePageComponentContainer flex flex-col items-center h-screen">
+      <div className="titleContainer flex flex-col h-24 justify-center text-center w-screen">
         <h1>Welcome to 5kstats.</h1>
       </div>
 
-      <div className="flex flex-col h-96 justify-center text-center my-3 px-2 rounded-lg border-solid border-2 border-indigo-600 ">
-        A platform for curated sports stats.
+      <div className="flex flex-col h-96 w-2/3 justify-center text-center my-3 px-2 rounded-lg border-solid border-2 border-slate-300 ">
+        <h3>A platform for curated sports stats built with ReactJS.</h3>
         <br />
-        Built with ReactJS.
-        <br />
-        NBA, NHL, and PGA insights.
+        <h3>NBA, NHL, and PGA insights.</h3>
       </div>
 
       <div className="flex flex-row flex-wrap justify-around w-screen h-auto my-5 mx-10 overflow-auto">
         {tiles.map((item) => (
-          <div class="w-48 h-48 my-5 rounded-lg border-solid border-2 border-indigo-600 flex flex-col justify-center text-center">
-            {item.title}
+          <div class="w-48 h-48 my-5 rounded-lg border-solid  flex flex-col justify-center text-center border-2 border-slate-300">
+            <h4>{item.title}</h4>
             <br />
-            {item.subtitle}
+            <h4>{item.subtitle}</h4>
           </div>
         ))}
       </div>
