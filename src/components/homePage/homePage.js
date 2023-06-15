3538dba1d3c568ca4db0c0e5b92de128df853161
JOEL_SMITH <joelsmith5000@gmail.com>
2023-06-14 22:13:18 -0700

    fix league tiles

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./homePage.css";
+import NBA from "../../images/logos/NBA_logo.png";
+import NHL from "../../images/logos/NHL_logo.png";
+import PGA from "../../images/logos/PGA_logo.png";
 
 function HomePage() {
   const tiles = [
-    { title: "NBA", subtitle: "Coming Soon", image: "bg-NBA-logo" },
-    { title: "NHL", subtitle: "Coming Soon", image: "bg-NHL-logo" },
-    { title: "PGA", subtitle: "", image: "bg-PGA-logo"},
+    { title: "NBA", subtitle: "Coming Soon", image: NBA },
+    { title: "NHL", subtitle: "Coming Soon", image: NHL },
+    { title: "PGA", subtitle: "", image: PGA },
   ];
 
   return (
@@ -23,9 +26,14 @@ function HomePage() {
         {tiles.map((item) => (
           <div
             key={item.title}
-            // className="w-48 h-48 my-5 rounded-lg border-solid flex flex-col justify-center text-center mx-3 border-2 border-slate-300 bg-NHL-logo"
-            className={`${item.image} bg-contain bg-no-repeat bg-center w-48 h-48 my-5 rounded-lg border-solid flex flex-col justify-center text-center mx-3 border-2 border-slate-300`}
+            className={
+              "w-48 h-48 my-5 rounded-lg border-solid flex flex-col content-center justify-center text-center mx-3 border-2 border-slate-300"
+            }
           >
+            <img
+              src={item.image}
+              className="object-scale-down h-36 w-36 mx-auto"
+            ></img>
             <h4>{item.subtitle}</h4>
           </div>
         ))}
