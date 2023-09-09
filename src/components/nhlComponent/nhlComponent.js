8933a8d57167ac0718a6558c3933f76e78542a4e
JOEL_SMITH <joelsmith5000@gmail.com>
2023-09-09 16:14:30 -0700

    Connect nhl page to api

--- src/components/nhlComponent/nhlComponent.js
+++ src/components/nhlComponent/nhlComponent.js
@@ -3,8 +3,39 @@ import "./nhlComponent.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import NhlDataService from "../../services/nhl";
 
 function NhlComponent() {
+
+  const [nhlAggregates, setNhlAggregates] = useState({});
+  const [nhlGoalieStats, setNhlGoalieStats] = useState([]);
+  // const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getNhlAggregates();
+    getNhlGoalieStats();
+  }, []);
+
+  const getNhlAggregates = () => {
+    NhlDataService.getNhlAggregates()
+      .then((response) => {
+        setNhlAggregates(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const getNhlGoalieStats = () => {
+    NhlDataService.getNhlGoalieStats()
+      .then((response) => {
+        setNhlGoalieStats(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen nhlComponentContainer h-screen">
       <div>
@@ -12,8 +43,18 @@ function NhlComponent() {
       </div>
 
       <div>
-        <h1>2023/2024 SEASON</h1>
+        <h1>Goals Against Breakdown</h1>
       </div>
+      <div>
+        <h1>Total Players Analyzed: {nhlAggregates.num_players}</h1>
+      </div>
+      <div>
+        <h1>Total Goals analyzed: {nhlAggregates.GA}</h1>
+      </div>
+      <div>
+        <h1>Coming Soon..</h1>
+      </div>
+
     </div>
   );
 }
