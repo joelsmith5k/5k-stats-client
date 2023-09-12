14dbdb84255abddf403a485a49049bf95c975061
JOEL_SMITH <joelsmith5000@gmail.com>
2023-09-11 21:33:42 -0700

    chart.js setup and initial test

--- src/components/nhlComponent/nhlComponent.js
+++ src/components/nhlComponent/nhlComponent.js
@@ -4,8 +4,32 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import NhlDataService from "../../services/nhl";
+import PieChart from "../common/charts/pie";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+
+Chart.register(CategoryScale);
 
 function NhlComponent() {
+
+  const [sampleChartData, setChartData] = useState({
+    labels: ["Left Wing", "Center", "Right Wing", "Defense"],
+    datasets: [
+      {
+        label: "Goals By Position",
+        data: [2002, 3664, 1444, 1279],
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
 
   const [nhlAggregates, setNhlAggregates] = useState({});
   const [nhlGoalieStats, setNhlGoalieStats] = useState([]);
@@ -54,6 +78,10 @@ function NhlComponent() {
       <div>
         <h1>Coming Soon..</h1>
       </div>
+
+      <div className="test">
+      <PieChart chartData={sampleChartData} />
+    </div>
 
     </div>
   );
