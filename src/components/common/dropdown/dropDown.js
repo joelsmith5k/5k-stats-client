39491cf1da969013cd27c4cd9d7ceca6fa438b93
JOEL_SMITH <joelsmith5000@gmail.com>
2023-12-03 11:10:07 -0800

    Fixing nhl dropdown layout issues

--- src/components/common/dropdown/dropDown.js
+++ src/components/common/dropdown/dropDown.js
@@ -18,7 +18,7 @@ function DropDownComponent({ items, onChange }) {
   };
 
   return (
-    <div className="my-10 w-80">
+    <div className="py-10 w-80">
       <Listbox value={selected} onChange={setSelected}>
         {({ open }) => (
           <>
