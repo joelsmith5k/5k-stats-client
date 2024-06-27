5c4998e66a946ddc06099d4b81d91b28df83cd30
JOEL_SMITH <joelsmith5000@gmail.com>
2024-06-26 18:57:48 -0700

    starting cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import PgaComponent from "./components/pgaComponent/pgaComponent";
 import NhlComponent from "./components/nhlComponent/nhlComponent";
